27075c562d1d4218ff302b36525b4c17f4bc0531
Colin Sullender <shiruken343@gmail.com>
2024-02-21 15:14:42 -0700

    Basic implementation of comment removals

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Devvit, FormOnSubmitEvent } from "@devvit/public-api";
 
 Devvit.configure({
+  redditAPI: true,
   redis: true,
 });
 
@@ -21,7 +22,7 @@ const form = Devvit.createForm((data) => {
         name: "post_id",
         label: "Post ID",
         type: "string",
-        defaultValue: data.id,
+        defaultValue: data.post_id,
         disabled: true,
       },
     ],
@@ -35,12 +36,14 @@ type RestrictedPostSettings = {
 
 async function formHandler(event: FormOnSubmitEvent, context: Devvit.Context) {
   const settings = event.values as RestrictedPostSettings;
-  console.log(settings);
+  const mod = await context.reddit.getCurrentUser();
   if (settings.is_enabled) {
     context.redis.set(settings.post_id, JSON.stringify(settings));
+    console.log(`u/${mod.username} enabled flaired user only mode on ${settings.post_id}`);
     context.ui.showToast("Commenting restricted to flaired users");
   } else {
     context.redis.del(settings.post_id);
+    console.log(`u/${mod.username} disabled flaired user only mode on ${settings.post_id}`);
     context.ui.showToast("Commenting permitted from all users");
   }
 }
@@ -51,11 +54,45 @@ Devvit.addMenuItem({
   label: "Restrict to Flaired Users",
   description: "Restrict commenting on this post to only flaired users",
   onPress: (event, context) => {
-    console.log(event);
     const data = {
-      id: event.targetId,
+      post_id: event.targetId,
     };
     context.ui.showForm(form, data);
+  },
+});
+
+Devvit.addTrigger({
+  event: "CommentSubmit",
+  onEvent: async (event, context) => {
+    const comment = event.comment;
+    if (!comment) {
+      throw new Error("Comment object missing from event data");
+    }
+
+    const author = event.author;
+    if (!author || !author.flair) {
+      throw new Error("Author object missing from event data");
+    }
+
+    const value = await context.redis.get(comment.postId);
+    if (!value) {
+      return;
+    }
+    const settings: RestrictedPostSettings = JSON.parse(value);
+
+    if (author.flair.text == "") {
+      const commentAPI = await context.reddit.getCommentById(comment.id);
+      await commentAPI
+        .remove()
+        .then(() => console.log(`Removed ${comment.id} by u/${author.name}`))
+        .catch((e) => console.error(`Error removing ${comment.id} by u/${author.name}`, e));
+      await commentAPI
+        .addRemovalNote({
+          reasonId: "",
+          modNote: `Commenting restricted to flaired users on ${comment.postId}`,
+        });
+    }
+
   },
 });
 
