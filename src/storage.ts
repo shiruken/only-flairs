d2bb006eef1343f22751a42ad37b506331376be0
Colin Sullender <shiruken343@gmail.com>
2024-02-26 15:34:12 -0700

    Automatically disable post restrictions after 30 days

--- src/storage.ts
+++ src/storage.ts
@@ -3,7 +3,12 @@ import { PostSettings } from "./types.js";
 
 export async function storePostSettings(post_id: string, settings: PostSettings, context: Devvit.Context): Promise<void> {
   const value = JSON.stringify(settings);
-  await context.redis.set(post_id, value);
+  await context.redis
+    .set(post_id, value)
+    .catch((e) => console.error(`Error writing ${post_id} to Redis`, e));
+  await context.redis
+    .expire(post_id, 60*60*24*30) // 30 days
+    .catch((e) => console.error(`Error setting expiration for ${post_id} in Redis`, e));
 }
 
 export async function getPostSettings(post_id: string, context: Devvit.Context | TriggerContext): Promise<PostSettings | undefined> {
@@ -15,7 +20,9 @@ export async function getPostSettings(post_id: string, context: Devvit.Context |
 }
 
 export async function clearPostSettings(post_id: string, context: Devvit.Context): Promise<void> {
-  await context.redis.del(post_id);
+  await context.redis
+    .del(post_id)
+    .catch((e) => console.error(`Error deleting ${post_id} in Redis`, e));
 }
 
 export async function isPostSettingsEdit(post_id: string, context: Devvit.Context): Promise<boolean> {
